c4363b163a8108bf91b9fe89ef34a9265747cefc
Dan Janosik <dan@47.io>
2021-09-20 19:28:06 -0400

    try/catch for error loading (edit)

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -387,32 +387,38 @@ router.post("/link/:linkId/edit", asyncHandler(async (req, res, next) => {
 		}
 
 		if (imgBuffer) {
-			let ciphertextFull = encryptor.encrypt(imgBuffer);
-			//await s3Bucket.put(ciphertextFull, `img/${linkId}/full`);
-			console.log(`imgFull: ${utils.descBuffer(imgBuffer)}`);
+			try {
+				let ciphertextFull = encryptor.encrypt(imgBuffer);
+				//await s3Bucket.put(ciphertextFull, `img/${linkId}/full`);
+				console.log(`imgFull: ${utils.descBuffer(imgBuffer)}`);
 
-			let buffer0 = await sharp(imgBuffer).resize({width: appConfig.images.widths[0], fit: "inside"}).toFormat("jpeg").toBuffer();
-			console.log(`img0: ${utils.descBuffer(buffer0)}`);
-			let ciphertext0 = encryptor.encrypt(buffer0);
-			await s3Bucket.put(ciphertext0, `img/${linkId}/w${appConfig.images.widths[0]}`);
+				let buffer0 = await sharp(imgBuffer).resize({width: appConfig.images.widths[0], fit: "inside"}).toFormat("jpeg").toBuffer();
+				console.log(`img0: ${utils.descBuffer(buffer0)}`);
+				let ciphertext0 = encryptor.encrypt(buffer0);
+				await s3Bucket.put(ciphertext0, `img/${linkId}/w${appConfig.images.widths[0]}`);
 
-			for (let i = 1; i < appConfig.images.widths.length; i++) {
-				let bufferX = await sharp(imgBuffer).resize({width: appConfig.images.widths[i], fit: "inside"}).toFormat("jpeg").toBuffer();
-				console.log(`img_${i}: ${utils.descBuffer(bufferX)}`);
-				let ciphertextX = encryptor.encrypt(bufferX);
-				s3Bucket.put(ciphertextX, `img/${linkId}/w${appConfig.images.widths[i]}`);
+				for (let i = 1; i < appConfig.images.widths.length; i++) {
+					let bufferX = await sharp(imgBuffer).resize({width: appConfig.images.widths[i], fit: "inside"}).toFormat("jpeg").toBuffer();
+					console.log(`img_${i}: ${utils.descBuffer(bufferX)}`);
+					let ciphertextX = encryptor.encrypt(bufferX);
+					s3Bucket.put(ciphertextX, `img/${linkId}/w${appConfig.images.widths[i]}`);
+				}
+
+				
+
+				link.hasImage = true;
+
+				const linksCollection = await db.getCollection("links");
+				const updateResult = await linksCollection.updateOne({_id:ObjectId(linkId)}, {$set: link});
+
+
+				req.session.userMessage = updateResult.modifiedCount == 1 ? "Link saved." : ("Status unknown: " + JSON.stringify(updateResult));
+				req.session.userMessageType = "success";
+
+			} catch (err) {
+				utils.logError("1238234yee", err);
 			}
-
 			
-
-			link.hasImage = true;
-
-			const linksCollection = await db.getCollection("links");
-			const updateResult = await linksCollection.updateOne({_id:ObjectId(linkId)}, {$set: link});
-
-
-			req.session.userMessage = updateResult.modifiedCount == 1 ? "Link saved." : ("Status unknown: " + JSON.stringify(updateResult));
-			req.session.userMessageType = "success";
 
 			res.redirect(`/link/${linkId}`);
 
