ebcb21dc8b9492672aaf4804702e00343b8708fa
Dan Janosik <dan@47.io>
2021-10-20 16:03:20 -0400

    migration update for server v5

--- docs/mongo-migration-links-to-items.js
+++ docs/mongo-migration-links-to-items.js
@@ -2,13 +2,13 @@
 db.links.renameCollection("items", false);
 
 // set all items.version="1.0"
-db.items.update({}, {$set:{"version":"1.0"}}, false, true);
+db.items.updateMany({}, {$set:{"version":"1.0"}});
 
 // rename property: item.desc -> item.text
-db.items.update({"version":"1.0", "desc": {$exists: true}}, {$rename:{"desc":"text"}}, false, true);
+db.items.updateMany({"version":"1.0", "desc": {$exists: true}}, {$rename:{"desc":"text"}});
 
 // set default item.type=link
-db.items.update({"version":"1.0", "type":{$exists:false}}, {$set:{"type":"link"}}, false, true);
+db.items.updateMany({"version":"1.0", "type":{$exists:false}}, {$set:{"type":"link"}});
 
 // set default item.imageSizes=["w350", "w500"], where applicable
-db.items.update({"hasImage":true, "version":"1.0", "imageSizes":{$exists:false}}, {$set:{"imageSizes":["w350", "w500"]}}, false, true);
+db.items.updateMany({"hasImage":true, "version":"1.0", "imageSizes":{$exists:false}}, {$set:{"imageSizes":["w350", "w500"]}});
